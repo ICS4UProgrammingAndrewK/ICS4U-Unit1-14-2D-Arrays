a291ec810df6efdb369c1dd435000da55f18e7ec
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-10-28 12:46:47 -0400

    Update 2DArrays.java

--- 2DArrays.java
+++ 2DArrays.java
@@ -1,7 +1,8 @@
-import java.util.Scanner;
+import java.util.*;
+import java.util.Random;
 
 /**
- * Class 2D Arrays
+ * Class TArrays
  */
  
 /**
@@ -9,33 +10,25 @@ import java.util.Scanner;
 * @version 1.0
 * @since   2020-10-17
 */
-public class 2DArrays {
+public class TArrays {
   public static void main(String[] args) 
   {
-    //This read the input from the console
-    Scanner userInput = new Scanner(System.in);
+    //this create the Array
+    int [][] number = new int [10][10];
     
-    //This diclare the int
-    int Assignment;
-    
-    // declare and construct a 2D array
-    int[][] gradeTable = 
-    { 
-      {99, 42, 74, 83, 50, 77}, 
-      {90, 91, 72, 88, 95, 83},
-      {88, 61, 74, 89, 96, 50},
-      {61, 89, 82, 98, 93, 78},
-    };
-    
-      //
-      System.out.println("grade 0,0: " + gradeTable[0][0]);
-      System.out.println("grade 2,4: " + gradeTable[2][4]);
-      
-      //
-      gradeTable[3][4] = 89 ;
-      
-      //This display the assignment
-      Assignment = gradeTable[0][0] + gradeTable[0][2] ;
-      System.out.println( "sum: " + Assignment );
+    //
+    for ( int c = 0; c = < number.length; c++) {
+      for( int r = 0; row < number [c].length; r++) {
+        number[c][r] = ( r + 1) + (c * 10);
+      }
+    }
+    //
+    for (int c = 0; c < number.length; c++) {
+      for ( int r = 0; r < number[c].length; r++) {
+        System.out.print(numbers[c][r] + " ");
+        
+      }
+      System.out.println("");
+    }
   }
 }
